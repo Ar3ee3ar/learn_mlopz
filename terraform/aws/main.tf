1127cc10e83dce8ca5bfa81f3d33743d36f614a0
Arzeezar <72872978+Arzeezar@users.noreply.github.com>
2025-10-02 17:26:32 +0700

    update terraform: user_data & dns name

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -4,15 +4,30 @@ provider "aws" {
     region = "ap-southeast-2"
 }
 
-# 3. Create vpc (our pvc instead of aws_default_vpc)
+# 3. Create vpc (our pvc instead of aws_default_vpc) ----------------------------
 # create default vpc if one does not exist
+# Ensures DNS support and hostname assignment are enabled for instances within the VPC.
 resource "aws_vpc" "main" {
     cidr_block          = "10.0.0.0/16" # network class 10.0.0.0 | host 10.0.0.1 - 10.0.255.255
     instance_tenancy    = "default"
+    enable_dns_support   = true
+    enable_dns_hostnames = true
     tags = {
         Name = "default vpc"
     }
 }
+# 3.1 Associating a Public DNS Record with Elastic IP (EIP) using Route 53
+# create custom domain name to point to specific public IP address
+# Allocate Elastic IP(EIP)
+# - Allocates a static public IPv4 address (Elastic IP) to your AWS account
+resource "aws_eip" "web_eip" {
+    vpc = true
+    tags = {
+        Name = "web-eip"
+    }
+}
+
+# ---------------------------------------------------------------------------
 # 4. create a public subnet (need IGW for outbound) -----------------------
 # use data source to get all availability zones in region
 data "aws_availability_zones" "availability_zones" {} 
@@ -129,7 +144,7 @@ data "aws_ami" "amazon_linux_2" {
 # launch the ec2 instance
 resource "aws_instance" "ec2_instance" {
     ami                     = data.aws_ami.amazon_linux_2.id
-    instance_type           = "t3.micro"
+    instance_type           = "t4g.micro"
     subnet_id               = aws_subnet.default_az1.id
     vpc_security_group_ids  = [aws_security_group.ec2_security_group.id]
     key_name                = aws_key_pair.ec2_key.key_name
@@ -144,13 +159,37 @@ resource "aws_instance" "ec2_instance" {
                 sudo systemctl enable docker
                 sudo yum install git -y
                 cd /home/ec2-user
-                git clone https://github.com/Ar3ee3ar/learn_mlopz.git
+                mkdir learn_mlopz
+                # git clone https://github.com/Ar3ee3ar/learn_mlopz.git
                 EOF
 
     tags = {
         Name = "docker server"
     }
 }
+
+# (Optional) Associate the EIP with an EC2 instance
+# - Associates the allocated EIP with a specific EC2 instance.
+resource "aws_eip_association" "eip_assoc" {
+    instance_id = aws_instance.ec2_instance.id
+    allocation_id = aws_eip.web_eip.id
+}
+
+# # 10. Create a Route 53 Hosted Zone (if you don't have one already)
+# # - Represents your domain's hosted zone in Route 53, where DNS records are managed.
+# resource "aws_route53_zone" "primary" {
+#     name = "mlopz.com" # domain name
+# }
+
+# # 10.1 Create a record to point your domain to the EIP
+# # - Creates an "A" record within the hosted zone, mapping a subdomain (e.g., www.example.com) to the public IPv4 address of your EIP.
+# resource "aws_route53_record" "www" {
+#     zone_id = aws_route53_zone.primary.zone_id
+#     name = "www.mlopz.com"
+#     type = "A"
+#     ttl  = 300
+#     records = [aws_eip.web_eip.public_ip]
+# }
 # --------------------------------------------------------------------------
 # (optional) connect to EC2 to send file/ run command/ etc.
 # an empty resource block
@@ -213,12 +252,12 @@ resource "aws_instance" "ec2_instance" {
 
 # }
 
-# output "instance_ip" {
-#     value  = aws_instance.ec2_instance.public_ip
-# }
+output "instance_ip" {
+    value  = aws_instance.ec2_instance.public_ip
+}
 
-# # print the url of the container
-# output "container_url" {
-#     value  = join("", ["http://", aws_instance.ec2_instance.public_dns])
-# }
+# print the url of the container
+output "container_url" {
+    value  = join("", ["http://", aws_instance.ec2_instance.public_dns])
+}
 
